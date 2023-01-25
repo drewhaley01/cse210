b7910cb86a46b9997e26827eb2f2709297478f7c
Drew Haley <drewhaley01@gmail.com>
2023-01-25 14:59:49 -0700

    journal loop

--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -4,6 +4,35 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop02 World!");
+        bool loop = true;
+while (loop == true)
+{ Console.WriteLine ("Please choose one of the following choices: ");
+    Console.WriteLine  ("1. Write");
+    Console.WriteLine ("2. Display");
+    Console.WriteLine ("3. Load");
+    Console.WriteLine ("4. Save");
+    Console.WriteLine ("5. Quit");
+    Console.WriteLine ("What would you like to do? ");
+
+    string userinput = Console.ReadLine() ?? "";
+
+    if (userinput == "1"){
+        Console.WriteLine("You have selected number 1");
+    }
+    else if (userinput == "2"){
+        Console.WriteLine("You have selected number 2");
+    }
+    else if (userinput == "3"){
+        Console.WriteLine("You have selected number 3");
+    }
+    else if (userinput == "4"){
+        Console.WriteLine("You have selected number 4");
+    }
+    else{
+        loop = false;
+    }
+
+
+}
     }
 }
